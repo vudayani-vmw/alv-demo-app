0adcd46183522a0871a3202d5135d3cd39ab27d6
V Udayani <vudayani@vudayani-a01.vmware.com>
2022-01-22 14:22:57 +0530

    configure cachemanager, http trace

--- src/main/java/com/empinventory/Application.java
+++ src/main/java/com/empinventory/Application.java
@@ -2,16 +2,28 @@ package com.empinventory;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -32,8 +44,24 @@ public class Application {
 		};
 	}
 	
-    	@Bean
+    @Bean
 	public HttpTraceRepository htttpTraceRepository() {
 		return new InMemoryHttpTraceRepository();
+	}
+
+	@Bean
+	@Primary
+	public CacheManager primaryCacheManager() {
+		return new ConcurrentMapCacheManager("vets", "addresses", "cities");
+	}
+
+	@Bean
+	public CacheManager sampleCacheManagerOneCacheManager() {
+		return new ConcurrentMapCacheManager("sampleCacheOne");
+	}
+	
+	@Scheduled(fixedRate = 1000000)
+	public void report() {
+		log.info("Employ Inventory ");
 	}
 }
